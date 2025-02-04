8d209ac0e36bc399b2e520a2fb73f4fc8babec25
Titus Saju <sajuti@mpac.ca>
2025-02-03 20:39:48 -0500

    feat: added UI for home

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Pressable, View, Text, ScrollView, ImageBackground } from "react-native";
+import { Pressable, View, Text, ScrollView, ImageBackground } from "react-native";
 import { Input, InputField, InputSlot } from "@/components/ui/input";
 
 import backgroundImage from "../assets/images/background.jpg";
@@ -7,11 +7,12 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, ButtonText } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <View className="relative">
-      <ImageBackground className="absolute h-[480px] w-full top-0" source={backgroundImage} />
+      <ImageBackground className="absolute h-full w-full top-0" source={backgroundImage} />
       <SafeAreaView className="px-4">
         <ScrollView className="relative h-full">
           <View className="flex-1 flex-row gap-2">
@@ -45,7 +46,7 @@ const Home = () => {
           <View className="flex flex-row justify-center items-center gap-2 mt-4 bg-[#FFFFFF25] p-3 px-4 rounded-full self-center">
             <Text className="text-lg text-white">Costco Canada L.L.C</Text>
           </View>
-          <View className="mt-16 flex flex-row">
+          <View className="mt-12 flex flex-row">
             <Pressable className="flex flex-col items-center w-1/4 p-2 rounded">
               <Ionicons name="add" className="bg-[#FFFFFF25] p-3 rounded-full" size={32} color={"white"} />
               <Text className="mt-3 text-white">Add Expense</Text>
@@ -62,6 +63,66 @@ const Home = () => {
               <Ionicons name="analytics" className="bg-[#FFFFFF25] p-3 rounded-full" size={32} color={"white"} />
               <Text className="mt-3 text-white">Analytics</Text>
             </Pressable>
+          </View>
+          <View className="mt-8 px-3">
+            <View className="flex flex-row justify-between items-center mb-6">
+              <View className="flex flex-row items-center gap-2">
+                <Ionicons name="albums-outline" size={24} color="#FFFFFF" />
+                <Text className="text-2xl text-white font-semibold">Recents</Text>
+              </View>
+
+              <Button variant="link" className="bg-[#FFFFFF25] px-3">
+                <ButtonText className="text-mdf text-white">View All</ButtonText>
+              </Button>
+            </View>
+
+            <View className="flex flex-col gap-5">
+              <Pressable className="bg-[#FFFFFF25] p-5 rounded flex flex-row justify-between items-center">
+                <View>
+                  <Text className="text-lg text-white">Costco Supplements</Text>
+                  <Text className="text-md text-white mt-1">Grocery</Text>
+                </View>
+                <View>
+                  <Text className="text-lg font-bold text-white">CA $ 300.00</Text>
+                </View>
+              </Pressable>
+              <Pressable className="bg-[#FFFFFF25] p-5 rounded flex flex-row justify-between items-center">
+                <View>
+                  <Text className="text-lg text-white">Costco Supplements</Text>
+                  <Text className="text-md text-white mt-1">Grocery</Text>
+                </View>
+                <View>
+                  <Text className="text-lg font-bold text-white">CA $ 300.00</Text>
+                </View>
+              </Pressable>
+              <Pressable className="bg-[#FFFFFF25] p-5 rounded flex flex-row justify-between items-center">
+                <View>
+                  <Text className="text-lg text-white">Costco Supplements</Text>
+                  <Text className="text-md text-white mt-1">Grocery</Text>
+                </View>
+                <View>
+                  <Text className="text-lg font-bold text-white">CA $ 300.00</Text>
+                </View>
+              </Pressable>
+              <Pressable className="bg-[#FFFFFF25] p-5 rounded flex flex-row justify-between items-center">
+                <View>
+                  <Text className="text-lg text-white">Costco Supplements</Text>
+                  <Text className="text-md text-white mt-1">Grocery</Text>
+                </View>
+                <View>
+                  <Text className="text-lg font-bold text-white">CA $ 300.00</Text>
+                </View>
+              </Pressable>
+              <Pressable className="bg-[#FFFFFF25] p-5 rounded flex flex-row justify-between items-center">
+                <View>
+                  <Text className="text-lg text-white">Costco Supplements</Text>
+                  <Text className="text-md text-white mt-1">Grocery</Text>
+                </View>
+                <View>
+                  <Text className="text-lg font-bold text-white">CA $ 300.00</Text>
+                </View>
+              </Pressable>
+            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
